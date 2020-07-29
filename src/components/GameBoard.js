7ab98b10561fe8c05621f959b1fbbd4b70d38a13
mcrd25 <mayacrd25@gmail.com>
2020-07-29 13:44:46 -0400

    Created placeShip, getBoard functions with helpers

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,54 +1,56 @@
 import Ship from './Ship';
 
 const Gameboard = () => {
-  const board = {
-    0: {
-      ship: new Ship(1),
-      direction: 'horizontal',
-    },
-    10: {
-      ship: new Ship(1),
-      direction: 'horizontal',
-    },
-    20: {
-      ship: new Ship(1),
-      direction: 'horizontal',
-    },
-    30: {
-      ship: new Ship(1),
-      direction: 'horizontal',
-    },
-    40: {
+  const board = Array(10).fill(null).map(() => Array(10).fill(null));
+  const ships = [
+    {
       ship: new Ship(2),
+      pos: [0, 0],
       direction: 'horizontal',
     },
-    50: {
-      ship: new Ship(2),
-      direction: 'horizontal',
-    },
-    60: {
-      ship: new Ship(2),
-      direction: 'horizontal',
-    },
-    70: {
-      ship: new Ship(3),
-      direction: 'horizontal',
-    },
-    80: {
-      ship: new Ship(3),
-      direction: 'horizontal',
-    },
-    90: {
-      ship: new Ship(4),
-      direction: 'horizontal',
-    },
-  };
-
-  const placeShip = (pos, ship, direction = 'horizontal') => {
+  ];
+  const spaceAvailable = (direction, x, y, ship) => {
     if (direction === 'horizontal') {
+      for (let i = y; i < ship.length; i += 1) {
+        if (board[x][i] !== null) {
+          return false;
+        }
+      }
+    } else {
+      for (let i = x; i < ship.length; i += 1) {
+        if (board[i][y] !== null) {
+          return false;
+        }
+      }
+    }
+    return true;
+  };
+  const placeShipHelper = (x, y, ship, direction) => {
+    if (direction === 'horizontal') {
+      for (let i = y; i < ship.length; i += 1) {
+        board[x][i] = ship;
+      }
+    } else {
+      for (let i = x; i < ship.length; i += 1) {
+        board[i][y] = ship;
+      }
     }
   };
+  const placeShip = (pos, ship, direction = 'horizontal') => {
+    const [x, y] = pos;
+    if (spaceAvailable(direction, x, y, ship)) {
+      placeShipHelper(x, y, ship, direction);
+      return true;
+    }
+    return false;
+  };
 
+  const getBoard = () => board;
+
+  return {
+    placeShip,
+    getBoard,
+  };
 };
 
 export default Gameboard;
