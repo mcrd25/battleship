af2ed95cc0dce58de810bbc3ba74e97a035feab7
mcrd25 <mayacrd25@gmail.com>
2020-07-29 16:00:13 -0400

    Removed console.log

--- tests/gamboard.test.js
+++ tests/gamboard.test.js
@@ -65,7 +65,6 @@ describe('allSunk', () => {
     gameboard3.receiveAttack([0, 0]);
     gameboard3.receiveAttack([3, 3]);
     gameboard3.receiveAttack([4, 3]);
-    console.log(gameboard3.getBoard());
     expect(gameboard3.allSunk()).toBe(true);
   });
 });
