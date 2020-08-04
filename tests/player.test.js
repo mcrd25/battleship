bf7b3f52c5e98c31b0cd59b717e2ae30d20f77b9
mcrd25 <mayacrd25@gmail.com>
2020-08-04 15:43:09 -0400

    Complete getBoard tests

--- tests/player.test.js
+++ tests/player.test.js
@@ -1,5 +1,6 @@
 import Player from '../src/components/Player';
 import Gameboard from '../src/components/GameBoard';
+import Ship from '../src/components/Ship';
 
 const playerName = 'Player1';
 const player1 = new Player(playerName);
@@ -25,6 +26,8 @@ describe('getBoard', () => {
   });
   test('any changes made in to board outside is reflected in board in Player component', () => {
     const tempBoard = player1.getBoard();
-
+    const ship = new Ship(2);
+    tempBoard.placeShip(ship, [0, 0]);
+    expect(player1.getBoard().getBoard()[0][0].ship).toBe(ship);
   });
 });
