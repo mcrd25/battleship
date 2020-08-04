ffdc2ebc7c80a7228cecb90bbf0caf28ec70b176
mcrd25 <mayacrd25@gmail.com>
2020-08-04 14:34:52 -0400

    Added player GetName() tests

--- tests/player.test.js
+++ tests/player.test.js
@@ -1,13 +1,19 @@
 import Player from '../src/components/Player';
 import Gameboard from '../src/components/GameBoard';
 
-
-const player1 = new Player('Player1');
+const playerName = 'Player1';
+const player1 = new Player(playerName);
 const defaultPlayer = new Player();
 const computerPlayer = new Player('computer', true);
 
 describe('getName', () => {
   test('returns name of player passed as first property on initialisation', () => {
-    
+    expect(player1.getName()).toBe(playerName);
+  });
+  test('not return default player name of if first property passed on initialisation', () => {
+    expect(player1.getName()).not.toBe('Player');
+  });
+  test('return default name when no property passed', () => {
+    expect(defaultPlayer.getName()).toBe('Player');
   });
 });
