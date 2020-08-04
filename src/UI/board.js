9a2eff83b456cf83765ff90090e2b567bb4b9eb6
mcrd25 <mayacrd25@gmail.com>
2020-08-04 18:30:15 -0400

    Initial battel grid created and style setup

--- src/UI/board.js
+++ src/UI/board.js
@@ -1,4 +1,34 @@
-const boardUI = () => {
+const BoardUI = (gridSize = 10) => {
+  const createCell = (className, id) => {
+    const cell = document.createElement('div');
+    cell.className = className;
+    cell.id = id;
+    return cell;
+  };
 
+  const createGameboardDiv = (id, className) => {
+    const gameboardDiv = document.createElement('div');
+    gameboardDiv.className = className;
+    gameboardDiv.id = id;
+    return gameboardDiv;
+  };
+
+  const drawGrid = (id, className) => {
+    const grid = document.createElement('div');
+    grid.id = id;
+    grid.className = className;
+    for (let x = 0; x < gridSize; x += 1) {
+      for (let y = 0; y < gridSize; y += 1) {
+        grid.appendChild(createCell('cell', `${x}-${y}`));
+      }
+    }
+    return grid;
+  };
+
+  return {
+    createGameboardDiv,
+    drawGrid,
+  };
 };
-export default boardUI;
+
+export default BoardUI;
--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
 import Gameboard from './components/GameBoard';
 import Player from './components/Player';
 import Ship from './components/Ship';
+import BoardUI from './UI/board';
+import M from 'materialize-css';
+import './sass/style.scss';
+
+
+const boardDOM = new BoardUI();
+
+const init = () => {
+  const content = document.querySelector('#content');
+  const boardsDiv = boardDOM.createGameboardDiv('boards', 'boards-div');
+  const grid = boardDOM.drawGrid('player');
+  boardsDiv.appendChild(grid);
+  content.appendChild(boardsDiv);
+};
+init();
