708933a59fa580ce32a9a934e4f4ee79671e2dba
mcrd25 <mayacrd25@gmail.com>
2020-08-04 14:17:59 -0400

    Adjusted random functions

--- src/components/Player.js
+++ src/components/Player.js
@@ -7,11 +7,11 @@ const Player = (name, board, computer = false) => {
   const getBoard = () => board;
 
   const makeRandomMove = () => {
-    let x = Math.random(10);
-    let y = Math.random(10);
+    let x = Math.floor(Math.random() * 10);
+    let y = Math.floor(Math.random() * 10);
     while (moves.includes([x, y])) {
-      x = Math.random(10);
-      y = Math.random(10);
+      x = Math.floor(Math.random() * 10);
+      y = Math.floor(Math.random() * 10);
     }
     moves.push([x, y]);
     return [x, y];
