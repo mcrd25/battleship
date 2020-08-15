6d729e2a7683c08e407e0c0f6ef2a91d574cde7d
mcrd25 <mayacrd25@gmail.com>
2020-08-14 21:09:03 -0400

    Player moves and ability to win added

--- src/UI/board.js
+++ src/UI/board.js
@@ -13,16 +13,33 @@ const BoardUI = (gridSize = 10) => {
     return gameboardDiv;
   };
 
-  const drawGrid = (id, className) => {
+  const drawGrid = (id, className, board, computer = false) => {
     const grid = document.createElement('div');
     grid.id = id;
     grid.className = className;
+
     for (let x = 0; x < gridSize; x += 1) {
       for (let y = 0; y < gridSize; y += 1) {
         const cell = createCell('cell', `${x}-${y}`);
-        cell.addEventListener('click', () => {
-          cell.className += ' blue';
-        });
+        if (computer) {
+          cell.addEventListener('click', () => {
+
+            const [x2, y2] = cell.id.split('-');
+            console.log(x2);
+            console.log(y2);
+            if (board.receiveAttack([x2, y2])) {
+              console.log('hit');
+              cell.className += ' red';
+              if (board.allSunk()) {
+                const display = document.querySelector('#display');
+                display.innerHTML = 'You Won!';
+              }
+            } else {
+              console.log('seA');
+              cell.className += ' blue';
+            }
+          });
+        }
         grid.appendChild(cell);
       }
     }
--- src/app.js
+++ src/app.js
@@ -5,15 +5,40 @@ import BoardUI from './UI/board';
 import M from 'materialize-css';
 import './sass/style.scss';
 
-
 const boardDOM = new BoardUI();
+const player1 = new Player('Player1');
+const computer = new Player('computer', true);
+computer.getBoard().setDefaultBoard();
+
+const move  = () => {
+  const turn = player1.getName();
+  const display = document.querySelector('#display');
+  if (turn === 'computer') {
+    display.innerHTML = 'Computer\'s turn';
+    const compMove = computer.makeRandomMove();
+    player1.getBoard().receiveAttack(compMove);
+    const cell = document.getElement
+  } else {
+    display.innerHTML = `${player1.getName()}'s turns`;
+  }
+
+}
 
 const init = () => {
   const content = document.querySelector('#content');
   const boardsDiv = boardDOM.createGameboardDiv('boards', 'boards-div');
-  const grid = boardDOM.drawGrid('player', 'grid');
-  const grid2 = boardDOM.drawGrid('computer', 'grid');
-  boardsDiv.append(grid, grid2);
+  const grid = boardDOM.drawGrid('player', 'grid', player1.getBoard());
+  const grid2 = boardDOM.drawGrid('computer', 'grid', computer.getBoard(), true);
+  const defaultBoard = document.createElement('button');
+  const display = document.createElement('div');
+  display.id = 'display';
+  defaultBoard.className = 'btn waves-effect waves-light use-default';
+  defaultBoard.innerHTML = 'Use Default';
+  defaultBoard.addEventListener('click', () => {
+    player1.getBoard().setDefaultBoard();
+  });
+  content.appendChild(defaultBoard);
+  boardsDiv.append(grid, display, grid2);
   content.appendChild(boardsDiv);
 };
 init();
