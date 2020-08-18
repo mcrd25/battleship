be035b9310cb25f77c109032f4f8048e1282167a
mcrd25 <mayacrd25@gmail.com>
2020-08-18 15:54:55 -0400

    Got placements to work

--- src/UI/placement.js
+++ src/UI/placement.js
@@ -1,11 +1,50 @@
 const PlacementUI = () => {
-  const drawPlacement = (ship) => {
+  const drawPlacement = (shipName, ship, player, length = 2) => {
     const xInput = document.createElement('input');
     const yInput = document.createElement('input');
-    xInput.id = `${ship}-x`;
-    yInput.id = `${ship}-y`;
+    const xLabel = document.createElement('label');
+    const yLabel = document.createElement('label');
+    const shipDiv = document.createElement('div');
+    const placeButton = document.createElement('button');
+
+    placeButton.id = shipName;
+    placeButton.className = 'btn waves-effect waves-light use-default';
+    placeButton.innerHTML = 'place';
+    placeButton.addEventListener('click', () => {
+      const x = parseInt(document.getElementById(`${placeButton.id}-x`).value, 10);
+      const y = parseInt(document.getElementById(`${placeButton.id}-y`).value, 10);
+      const direction = 'vertical';
+      if (player.getBoard().placeShip(ship, [x, y], direction)) {
+        placeButton.className += 'disabledDiv';
+        if (direction === 'horizontal') {
+          for (let i = 0; i < ship.length; i += 1) {
+            const cell = document.getElementById(`${x}-${y + i}-p`);
+            cell.className += ' ship';
+          }
+        } else {
+          for (let i = 0; i < ship.length; i += 1) {
+            const cell = document.getElementById(`${x + i}-${y}-p`);
+            cell.className += ' ship';
+          }
+        }
+      }
+    });
+
+    for (let i = 0; i < length; i += 1) {
+      const cell = document.createElement('div');
+      cell.className = 'single-cell';
+      shipDiv.appendChild(cell);
+    }
+
+    xInput.id = `${shipName}-x`;
+    xLabel.htmlFor = xInput.id;
+    xLabel.innerHTML = 'x';
+    yInput.id = `${shipName}-y`;
+    yLabel.for = yInput.id;
+    yLabel.innerHTML = 'y';
     xInput.className = 'placement-input';
-    return [xInput, yInput];
+    yInput.className = 'placement-input';
+    return [xInput, xLabel, yInput, yLabel, shipDiv, placeButton];
   };
 
   return {
--- src/app.js
+++ src/app.js
@@ -6,7 +6,62 @@ import PlacementUI from './UI/placement';
 import M from 'materialize-css';
 import './sass/style.scss';
 
+M.AutoInit();
+
 const boardDOM = new BoardUI();
+const ships = [
+  {
+    ship: new Ship(1),
+    pos: [0, 0],
+    direction: 'horizontal',
+  },
+  {
+    ship: new Ship(1),
+    pos: [1, 0],
+    direction: 'horizontal',
+  },
+  {
+    ship: new Ship(1),
+    pos: [2, 0],
+    direction: 'horizontal',
+  },
+  {
+    ship: new Ship(1),
+    pos: [3, 0],
+    direction: 'horizontal',
+  },
+  {
+    ship: new Ship(2),
+    pos: [4, 0],
+    direction: 'horizontal',
+  },
+  {
+    ship: new Ship(2),
+    pos: [5, 0],
+    direction: 'horizontal',
+  },
+  {
+    ship: new Ship(2),
+    pos: [6, 0],
+    direction: 'horizontal',
+  },
+  {
+    ship: new Ship(3),
+    pos: [7, 0],
+    direction: 'horizontal',
+  },
+  {
+    ship: new Ship(3),
+    pos: [8, 0],
+    direction: 'horizontal',
+  },
+  {
+    ship: new Ship(4),
+    pos: [9, 0],
+    direction: 'horizontal',
+  },
+
+];
 const placementDOM = new PlacementUI();
 const player1 = new Player('Player1');
 const computer = new Player('computer', true);
@@ -56,6 +111,15 @@ const game = (playerGrid, computerGrid) => {
   }
 };
 
+const placementDivs = (ships, parent) => {
+  for (let i = 0; i < ships.length; i += 1) {
+    const placementDiv = document.createElement('div');
+    const [ship1x, labelx, ship1y, labely, shipDiv, placeButton] = placementDOM.drawPlacement(`ship${i + 1}`, ships[i].ship, player1, ships[i].ship.length);
+    placementDiv.append(shipDiv, labelx, ship1x, labely, ship1y, placeButton);
+    parent.appendChild(placementDiv);
+  }
+};
+
 const init = () => {
   const content = document.querySelector('#content');
   const boardsDiv = boardDOM.createGameboardDiv('boards', 'boards-div');
@@ -63,45 +127,16 @@ const init = () => {
   const grid2 = boardDOM.drawGrid('computer', 'grid disabledDiv', computer, player1, true);
   const defaultBoard = document.createElement('button');
   const display = document.createElement('div');
-  const placementDiv = document.createElement('div');
-  const placeButton = document.createElement('button');
-  const [ship1x, ship1y] = placementDOM.drawPlacement('ship1');
-
+  const instructionsDiv = document.createElement('div');
   display.id = 'display';
-  placeButton.id = 'ship1';
-  placeButton.className = 'btn waves-effect waves-light use-default';
-  placeButton.innerHTML = 'place';
-  placeButton.addEventListener('click', () => {
-    const x = parseInt(document.getElementById(`${placeButton.id}-x`).value, 10);
-    const y = parseInt(document.getElementById(`${placeButton.id}-y`).value, 10);
-    const ship = new Ship();
-    const direction = 'vertical';
-    if (player1.getBoard().placeShip(ship, [x, y], direction)) {
-      placeButton.className += 'disabledDiv';
-      if (direction === 'horizontal') {
-        for (let i = 0; i < ship.length; i += 1) {
-          const cell = document.getElementById(`${x}-${y + i}-p`);
-          cell.className += ' ship';
-        }
-      } else {
-        for (let i = 0; i < ship.length; i += 1) {
-          const cell = document.getElementById(`${x + i}-${y}-p`);
-          cell.className += ' ship';
-        }
-      }
-    }
-  });
-  placementDiv.append(ship1x, ship1y, placeButton);
   defaultBoard.className = 'btn waves-effect waves-light use-default';
   defaultBoard.innerHTML = 'Use Default';
-  content.appendChild(defaultBoard);
-  content.appendChild(placementDiv);
+  placementDivs(ships, instructionsDiv);
   boardsDiv.append(grid, display, grid2);
-  content.appendChild(boardsDiv);
+  content.append(instructionsDiv, boardsDiv);
   defaultBoard.addEventListener('click', () => {
     player1.getBoard().setDefaultBoard();
     grid2.className = 'grid';
-    // game(grid, grid2);
   });
 };
 init();
